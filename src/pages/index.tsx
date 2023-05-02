41175ef4d5549b15bbc5c6f51b71ad9620d08825
mnt3710 <tomato3710@gmail.com>
2023-05-03 02:39:42 +0900

    delete: コメントアウト部分

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -23,17 +23,6 @@ const fetchPrefList = async () => {
   return response.data
 }
 
-/* const fetchPopulation = async () => {
-  const url =
-    'https://opendata.resas-portal.go.jp/api/v1/population/composition/perYear?prefCode=11'
-  const header = {
-    headers: { 'X-API-KEY': process.env.API_KEY },
-  }
-  const response = await axios.get(url, header)
-  console.log(response.data)
-  return response.data
-} */
-
 const fetchPopulation = async (prefCode) => {
   const url = 'http://localhost:3000/api/population?prefCode=' + prefCode
   const response = await axios.get(url)
@@ -44,21 +33,17 @@ const Home = ({ prefList }) => {
   const [population, setPopulation] = useState([])
   const [year, setYear] = useState([])
 
-  let populationArray = []
-  let yearArray = []
-
   const clickBtn = async (prefCode, pref) => {
     const response = await fetchPopulation(prefCode)
     const responseArray = response.result.data[0].data
 
-    populationArray.push(...population ,{name: pref, data:[]})
-    yearArray = []
+    let populationArray = [...population, { name: pref, data: [] }]
+    let yearArray = []
 
     responseArray.forEach((x) => {
       populationArray[population.length].data.push(x.value)
       yearArray.push(x.year)
     })
-
 
     setPopulation(populationArray)
     setYear(yearArray)
@@ -102,9 +87,6 @@ const Home = ({ prefList }) => {
           year={year}
           fetchPopulation={clickBtn}
         />
-{/*         <button onClick={() => clickBtn(2, '北海道')}>push</button>
-        <button onClick={() => clickBtn(1, '山形')}>push</button>
-        <button onClick={() => console.log(population)}>disp</button> */}
       </main>
     </>
   )
